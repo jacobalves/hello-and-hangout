640b0b354e72e25e03ca5e3db96b02d56c0e1f72
Jacob Alves <jake.alves@zapatacomputing.com>
2020-12-02 17:45:42 -0500

    added env variables

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ This function will sit and wait based on a parameter that is passed in.
 
 from time import sleep
 import json
+import os
 
 def hello():
     message = "Hello from Orquestra!"
@@ -18,3 +19,11 @@ def hello():
 def hangout(duration):
     sleep(duration)
 
+def environment_variables():
+
+    message_dict = {}
+    message_dict["env-variables"] = message
+    message_dict["schema"] = "envvariables"
+
+    with open("envvars.json",'w') as f:
+        f.write(json.dumps(dict(os.environ), indent=2)) # W
