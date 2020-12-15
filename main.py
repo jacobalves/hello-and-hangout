da6beaa89ef7a3cd95cc47012114a1444008827f
Jacob Alves <jake.alves@zapatacomputing.com>
2020-12-15 11:08:37 -0500

    fixed main.py and added requirements.txt

--- main.py
+++ main.py
@@ -21,23 +21,22 @@ def hello():
 def hangout(duration):
     sleep(duration)
 
-def hello-translate(lang):
+def hello_translate(lang):
     message = TextBlob("Hello from Orquestra!")
     message_dict = {}
     if lang == 'en':
         message_dict["message"] = message.string
     else:
-        message_dict["message"] = message.translate(to=lang)
+        message_dict["message"] = message.translate(to=lang).string
     message_dict["schema"] = "message"
-
     with open("hello.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
 
 def environment_variables():
-
     message_dict = {}
     message_dict["env-variables"] = dict(os.environ)
     message_dict["schema"] = "envvariables"
-
     with open("envvars.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # W
+
+hello_translate("en")
